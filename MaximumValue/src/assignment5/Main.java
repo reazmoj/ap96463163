3bfbe216b90b70e7a10a96514dac1f5dc5f6deaa
mojtaba hashemi <44290553+reazmoj@users.noreply.github.com>
2019-04-20 21:55:04 +0430

    Add files via upload

---
+++ MaximumValue/src/assignment5/Main.java
@@ -0,0 +1,20 @@
+package assignment5;
+import  java.lang.Math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double f;
+        double max;
+        double min;
+        for (int x = -1000 ; x <= 1000 ; x++){
+            for (int y = -1000 ; y <= 1000 ; x++){
+                f = Math.sqrt(x*x + y*y) / ((x*Math.sin(x+y)) + (y*Math.cos(x+y)));
+
+            }
+        }
+    }
+    public static void maximumOfFanction(){
+
+    }
+}
